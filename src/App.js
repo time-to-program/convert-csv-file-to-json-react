7baae54b9065702aa1d014af1880f104151cec4d
raviteja-v <timeforprogramming@gmail.com>
2023-07-10 14:21:13 +0530

    Convert CSV file Data to JSON

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [jsonData, setJsonData] = useState(null);
+
+  const convertCSVToJson = (csvData) => {
+    const lines = csvData.split("\n");
+    const headers = lines[0].split(",");
+    const result = [];
+
+    for (let i = 1; i < lines.length; i++) {
+      const obj = {};
+      const currentLine = lines[i].split(",");
+
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j].trim()] = currentLine[j].trim();
+      }
+
+      result.push(obj);
+    }
+
+    return result;
+  };
+
+  const handleCSVInputChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const csvData = e.target.result;
+      const jsonData = convertCSVToJson(csvData);
+      setJsonData(jsonData);
+    };
+
+    reader.readAsText(file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" accept=".csv" onChange={handleCSVInputChange} />
+
+      {jsonData ? (
+        <div className="json-container">
+          <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+        </div>
+      ) : (
+        <p>Please select a CSV file.</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
